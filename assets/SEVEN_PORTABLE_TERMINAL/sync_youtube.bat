b952e17c611a95e69723b26f5a7bc1567e740703
BlueAzur-Hub <Blue.Azur@protonmail.com>
2026-05-24 04:32:19 +0200

    Add automatic YouTube sync to Seven Heaven

--- assets/SEVEN_PORTABLE_TERMINAL/sync_youtube.bat
+++ assets/SEVEN_PORTABLE_TERMINAL/sync_youtube.bat
@@ -1,17 +1,19 @@
 @echo off
-cd /d "%~dp0"
+title Seven Heaven - YouTube Sync
 
 echo =========================================
 echo SEVEN HEAVEN - YOUTUBE SYNC
 echo =========================================
+echo.
 
 cd /d C:\Aerith_YouTube_Reader
 
-python youtube_reader_v3.py
+python export_blue_azur_youtube_data_v3.py
 
 echo.
 echo =========================================
 echo Synchronisation terminee
 echo =========================================
+echo.
 
 pause
