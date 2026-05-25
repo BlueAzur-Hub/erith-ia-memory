878b1e9230d7e15da327ff0315c6a2a042dc3621
BlueAzur-Hub <Blue.Azur@protonmail.com>
2026-05-25 23:37:43 +0200

    Castle in the Sky

--- assets/SEVEN_PORTABLE_TERMINAL/script.js
+++ assets/SEVEN_PORTABLE_TERMINAL/script.js
@@ -346,7 +346,7 @@ document.getElementById("toggleReadableBtn").addEventListener("click",()=>{docum
 document.getElementById("toggleTraceBtn").addEventListener("click",()=>{const panel=document.getElementById("advancedPanel"); panel.hidden=!panel.hidden; renderTrace();});
 
 const openRustDeskBtn = document.getElementById("openRustDeskBtn");
-const RYZEN7_RUSTDESK_LINK = "rustdesk://48841137";
+const RYZEN7_RUSTDESK_LINK = "seven-ryzen://open";
 
 if (openRustDeskBtn) {
   openRustDeskBtn.setAttribute("href", RYZEN7_RUSTDESK_LINK);
