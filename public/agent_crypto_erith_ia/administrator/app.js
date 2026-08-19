277780b3a10b699a203bc19572643e29c78a9c73
BlueAzur-Hub <Blue.Azur@protonmail.com>
2026-08-19 06:58:39 +0200

    agent-crypto: build 40.1.17 remove Market Flow label host and restore Technical Reading side dock

--- public/agent_crypto_erith_ia/administrator/app.js
+++ public/agent_crypto_erith_ia/administrator/app.js
@@ -12131,7 +12131,7 @@ function priceDeltaPct(nowAsset, prevAsset) { const a = Number(nowAsset?.price_e
 }
 
 const ATLAS_STABLE_STACK = Object.freeze({
-  interface: "Build 40.1.15",
+  interface: "Build 40.1.17",
   controlCenter: "V2.3.2R5",
   bridge: "V1.9.5",
   bridgeNumeric: "1.9.5",
@@ -40322,7 +40322,7 @@ function atlasSourceTruthBuild(contract) {
    ============================================================ */
 
 // Single manually edited version value.
-const ATLAS_BUILD = "40.1.15";
+const ATLAS_BUILD = "40.1.17";
 const ATLAS_DIRECT_5_5_STABLE_MS = 10000;
 const ATLAS_DIRECT_5_5_MIN_CHECKS = 3;
 
@@ -46993,8 +46993,8 @@ atlasRcStaticAudit = function atlasRcStaticAudit3812() {
 
 const ATLAS_RUNTIME_TRUTH_3813 = Object.freeze({
   schema: "agent_crypto_runtime_truth_v3813",
-  build: "40.1.15",
-  asset_token: "market-core-v2.0-alpha-build-40.1.15"
+  build: "40.1.17",
+  asset_token: "market-core-v2.0-alpha-build-40.1.17"
 });
 
 function atlasRuntimeTruth3813() {
