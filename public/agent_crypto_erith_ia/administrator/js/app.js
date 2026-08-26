19ce6e1222cfffdd061f27b474ea4fb325b8e702
BlueAzur-Hub <Blue.Azur@protonmail.com>
2026-08-26 14:31:26 +0200

    agent-crypto: 40.4.48 close Administrator migration legacy debt

--- public/agent_crypto_erith_ia/administrator/js/app.js
+++ public/agent_crypto_erith_ia/administrator/js/app.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const ADMIN_BUILD = "40.4.47";
+  const ADMIN_BUILD = "40.4.48";
   const ADMIN_RELEASE = "AETHER ORACLE TRUE LAZY PRESENTATION + RUNTIME PARITY LOCK";
   const ENGINE_BUILD = "38.15.11";
   const CLASSIC_WEB_BUILD = "38.15.13";
