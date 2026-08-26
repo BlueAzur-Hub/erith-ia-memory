244b60cec9da9416e6f78134709411456fe72b9e
BlueAzur-Hub <Blue.Azur@protonmail.com>
2026-08-26 02:21:00 +0200

    agent-crypto: build 40.4.23 seal Firefox operator acceptance and final checkpoint

--- public/agent_crypto_erith_ia/administrator/app.js
+++ public/agent_crypto_erith_ia/administrator/app.js
@@ -50441,7 +50441,7 @@ try {
 } catch (_) {}
 
 // Single manually edited version value.
-const ATLAS_BUILD = "40.4.22";
+const ATLAS_BUILD = "40.4.23";
 const ATLAS_DIRECT_5_5_STABLE_MS = 10000;
 const ATLAS_DIRECT_5_5_MIN_CHECKS = 3;
 
